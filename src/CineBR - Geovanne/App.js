9a9e998a0b839e14d3a00c052ea62b791e0a6bc5
geovannedomonte <129304075+geovannedomonte@users.noreply.github.com>
2024-04-03 19:38:30 -0300

    Add files via upload

---
+++ src/CineBR - Geovanne/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import TelaCinemas from './src/components/pages/TelaCinemas'
+
+const App = () => {
+  return (
+    <TelaCinemas />
+  )
+}
+
+export default App;
