9cfbc142f2e1e6e7bc5e0924f55096a9662dae94
bianca <125494623+biamoroll@users.noreply.github.com>
2024-04-06 18:25:25 -0300

    Add files via upload

---
+++ src/cinebr-tela-filme-Bianca/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import TelaFilme from './src/components/pages/TelaFilme'
+const App = () => {
+
+  return (
+  <TelaFilme />
+
+  );
+}
+
+export default App;
