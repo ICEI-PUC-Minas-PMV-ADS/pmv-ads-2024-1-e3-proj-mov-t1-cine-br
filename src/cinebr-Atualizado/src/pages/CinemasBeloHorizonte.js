6bc8f790414b43e019e7e4ac4570781bbc11fc4f
MayraRodriguez777 <mayyra.gabriella@gmail.com>
2024-05-20 15:50:09 +0100

    sss

--- src/cinebr-Atualizado/src/pages/CinemasBeloHorizonte.js
+++ src/cinebr-Atualizado/src/pages/CinemasBeloHorizonte.js
@@ -7,32 +7,24 @@ const CinemasBeloHorizonte = () => {
   const [selectedFilme, setSelectedFilme] = useState(null);
   const navigation = useNavigation();
 
-  const ingressos = [
-  { tipo: 'Inteira', codigo: '001', valor: 40.0 },
-  { tipo: 'Meia', codigo: '002', valor: 20.0 },
-  ];
-
   const filmes = [
     {
       nome: 'Animais Fantásticos: Os Segredos de Dumbledore',
       sinopse: 'O professor Albus Dumbledore sabe que o poderoso bruxo das trevas Gellert Grindelwald está se movendo para assumir o controle do mundo bruxo. Incapaz de detê-lo sozinho, ele confia a Newt Scamander um plano contra o inimigo.',
       imagem: 'https://m.media-amazon.com/images/S/pv-target-images/94d299d381ded85afac95ba386ef5557b6e737800be59f5337b017af05df339d.jpg',
       id: 1,
-      classificacao: '12',
     },
     {
       nome: 'Godzilla e Kong: O Novo Império',
       sinopse: 'Godzilla e o todo-poderoso Kong enfrentam uma ameaça colossal escondida nas profundezas do planeta, desafiando a sua própria existência e a sobrevivência da raça humana',
       imagem: 'https://cinemax.pt/wp-content/uploads/2024/01/godzila.jpg',
       id: 2,
-      classificacao: '12',
     },
     {
       nome: 'Kung Fu Panda 4',
       sinopse: 'Uma poderosa feiticeira que muda de forma coloca os olhos no Cajado da Sabedoria. Po de repente percebe que precisa de ajuda e logo descobre que heróis podem ser encontrados nos lugares mais inesperados.',
       imagem: 'https://br.web.img3.acsta.net/pictures/23/12/13/18/13/4592801.jpg',
       id: 3,
-      classificacao: 'Livre',
     },
   ];
 
@@ -74,10 +66,10 @@ const CinemasBeloHorizonte = () => {
           <View style={styles.modalView}>
             <Text style={styles.modalText}>{selectedFilme?.nome}</Text>
             <Image source={{ uri: selectedFilme?.imagem }} style={styles.modalImage} />
-            <Text style={styles.moreInfo}>{selectedFilme?.sinopse}</Text>
-            <Text style={styles.moreInfo}>Classificação Indicativa: {selectedFilme?.classificacao} anos</Text>
-            <Text style={styles.moreInfo}> Inteira: R$40,00 </Text>
-            <Text style={styles.moreInfo}>Meia: R$20,00</Text>
+            <Text>{selectedFilme?.sinopse}</Text>
+            <Text style={styles.moreInfo}> 
+              'Mais informações"
+            </Text>
             <View style={styles.buttonContainer}>
               <View style={styles.customButton}>
                 <Button title="Reservar Ingresso" flex-direction='row' color='#4DCEC1' margin='10' onPress={handleCompraPress} />
