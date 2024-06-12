f217fd5c8f00afe9c907a99cefb4c4ab4bc68e52
Mayra Gabriella Vitoriana Machado Rodriguez <160336811+MayraMachado77777@users.noreply.github.com>
2024-06-12 06:44:37 +0100

    Delete src/Paginas Separadas directory

--- src/Paginas Separadas/CineBr-Arthur/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CadastroPage from './src/pages/TelaCadastro';
-
-export default function App() {
-  return (
-    <CadastroPage />
-  );
-}
--- src/Paginas Separadas/cinebr-Peterson/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Filmes from './src/pages/Filmes';
-
-export default function App() {
-  return (
-    <Filmes />
-  );
-}
